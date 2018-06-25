8985a81d4377c54d4c8033cdf24591d791e1f77b
jpeek01 <jpeek01@gmail.com>
2018-06-24 23:52:37 -0500

    updating javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,62 @@
 
-var numberOfWins = 0;
-
 var game = {
     gameWords: ["gryffindor","hermione","hogwarts","quidditch","Snape","dumbledore","mcgonagall","longbottom","voldemort","accio","avadakedavra"],
     currentWord: "",
     currentWordMask: [],
     userGuesses: [],
-    allowableGuesses: 8,
-    incorrectGuesses: 0,
+    guessesRemaining: 1,
+    incorrectGuesses: 3,
 };
 
 game.currentWord = game.gameWords[Math.floor(Math.random() * game.gameWords.length)];
 
-var wordMasklength = 
+var numberOfWins = 0;
+var numberOfGuess;
+var userGuess;
+var gameBegin = true;
 
-document.onkeydown = function(event) {
-    // document.getElementById("currentGameDiv").style.display = "none";
-    console.log(event)
+game.guessesRemaining = game.currentWord.length;
+wordMask = mask(game.currentWord);
+
+document.onkeyup = function(event) {
+    console.log(event);
+
+    userGuess = event.key;
+
+    if (game.currentWord.indexOf(userGuess) === -1  && game.incorrectGuesses <= 3) {
+        alert("bad guess " + userGuess);
+        game.incorrectGuesses--;
+    } else if (userGuess == 'r' && game.incorrectGuesses <= 3) {
+        alert("it's in there")
+
+        updateMask(wordMask, userGuess, game.currentWord.indexOf(userGuess));
+    }
+    // } else {
+    //     alert("game over")
+    // }
+
     var html =
-    "<p>Your word is " + game.currentWord + "</p>" +
-    ""
+    '<p>Your word is ' + game.currentWord + '</p>' +
+    '<p>Your word is ' + wordMask + '</p>' +
+    '<p>' + game.incorrectGuesses + ' incorrect guesses remaining' + '</p>'
     ;
 
-  // Set the inner HTML contents of the #game div to our html string
   document.querySelector("#currentGameDiv").innerHTML = html;
 };
 
-document.onkeyup = function(event) {
-    
 
-};
+function mask(word) {
+      var masked = word.substring(0, word.length).replace(/[a-zA-Z]/g,"_")
+      return masked;
+    }
+
+function updateMask(masked, guess, position) {
+    masked = masked.substring(position).replace(/[_]/,guess);
+    alert("got here " + guess + masked );
+    return masked;
+    }
+
+
 
 
 
